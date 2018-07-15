a9431073f86a2b3502c6f99f506c580806b9141b
Ricardo GARCIA SANCHEZ <ricardo@rigasa.pro>
2018-07-15 17:01:10 +0200

    Install ionic canary and menu

---
+++ RiGaSa_Menu/e2e/src/app.po.ts
@@ -0,0 +1,15 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo(destination) {
+    return browser.get(destination);
+  }
+
+  getTitle() {
+    return browser.getTitle();
+  }
+
+  getPageOneTitleText() {
+    return element(by.tagName('app-page-home')).element(by.deepCss('ion-title')).getText();
+  }
+}
